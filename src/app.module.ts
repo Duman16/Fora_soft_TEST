82de9d141772a1be56ff6ab9f5e91a8e62ca0a02
Duman16 <duman29042006@gmail.com>
2026-01-23 00:02:20 +0500

    feat: update  main app module with TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,44 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CommentsModule } from './comments/comments.module';
+import { User } from './users/entities/user.entity';
+import { Task } from './tasks/entities/task.entity';
+import { Comment } from './comments/entities/comment.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Конфигурация окружения
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    // Подключение к PostgreSQL
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [User, Task, Comment],
+        synchronize: true, // В продакшене использовать миграции
+        logging: configService.get('NODE_ENV') === 'development',
+      }),
+      inject: [ConfigService],
+    }),
+
+    // Модули приложения
+    AuthModule,
+    UsersModule,
+    TasksModule,
+    CommentsModule,
+  ],
 })
 export class AppModule {}
